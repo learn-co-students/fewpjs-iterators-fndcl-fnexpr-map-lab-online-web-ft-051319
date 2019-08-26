28979bbc4296011d10d765770e5e6db675b83469
github username <github email address>
2019-08-25 21:38:31 -0500

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,25 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map( (tut) => {
+    let words_of_sentence = tut.split(" ");
+    return words_of_sentence.map( (word) => {
+           return word.slice(0, 1).toUpperCase() + word.slice(1);
+         }).join(" ");
+      
+  })
 }
+
+// const titleCased = tutorials.map(function(tut){
+
+//   let words_of_sentence = tut.split(" ");
+  
+
+//    return words_of_sentence.map(function(word){
+//      return word.slice(0, 1).toUpperCase() + word.slice(1);
+//    }).join(" ");
+
+
+// });
