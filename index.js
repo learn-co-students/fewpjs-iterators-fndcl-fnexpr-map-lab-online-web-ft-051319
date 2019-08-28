4b51440ef0bc6f08c61dd1c7870d1a0dd7de1dcc
wkpescherine <wkpescherine@gmail.com>
2019-08-28 16:30:12 -0400

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,22 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  let newTutorial = [];
+
+  function firstLetterToUpper(word){
+    word[0].toUpperCase()
+  }
+  for(let a = 0; a<tutorials.length; a++){
+    let res = tutorials[a].split(" ")
+    let newString = res.map(firstLetterToUpper)
+
+    newTutorial.push(newString)
+    // for(let b = 0; b<newString.length; b++){
+    //   newString[b][0].toUpperCase()
+    //   tutorials[a] = newString
+  
+    return newTutorial
+    //return tutorials
+  }
+
 }
